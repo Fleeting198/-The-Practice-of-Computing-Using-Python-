c459c872677f38cf38bbe156ac87e5ebffd8e0c3
Fleeting198 <646335299@qq.com>
2016-05-19 10:36:55 +0800

    Small fixes.

--- Chap07/programing/01.py
+++ Chap07/programing/01.py
@@ -1,17 +1,21 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
 
-def get_data_list(file_name):
+
+def getDataList(file_name):
+    """ 从csv中读取数据 """
     data_file = open(file_name, 'r')
     data_list = []
     for line in data_file:
         data_list.append(line.strip().split(','))
     return data_list[1:]
 
-def getMonthlyAverages(dataList):
+
+def getMonthlyAverages(data_list):
+    """ 计算每月平均值，返回元组列表 """
     lMonthlyAvg = []
     dict_tmp = {}
-    for dataOneDay in dataList:
+    for dataOneDay in data_list:
         thisDate = datetime.strptime(dataOneDay[0], '%Y-%m-%d')
         monthDate = thisDate.strftime('%Y-%m')
         if monthDate not in dict_tmp:
@@ -20,12 +24,14 @@ def getMonthlyAverages(dataList):
             dict_tmp[monthDate][0] += int(dataOneDay[5]) * float(dataOneDay[6])
             dict_tmp[monthDate][1] += float(dataOneDay[6])
 
-    for k, v in dict_tmp.iteritems():
+    for k, v in dict_tmp.items():
         lMonthlyAvg.append((v[0]/v[1], k))
 
     return lMonthlyAvg
 
+
 def printInfo(monthlyAveragesList):
+    # 按照平均值（元组的0号元素）降序排序
     lMonthlyAvg = sorted(monthlyAveragesList, key=lambda x: x[0], reverse=True)
 
     if len(lMonthlyAvg) > 6:
@@ -42,5 +48,6 @@ def printInfo(monthlyAveragesList):
         for data in lMonthlyAvg:
             print(data[1], '\t\t', str(round(data[0], 2)))
 
+
 if __name__ == '__main__':
-    printInfo(getMonthlyAverages(get_data_list('table.csv')))
+    printInfo(getMonthlyAverages(getDataList('table.csv')))
